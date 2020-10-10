47f1abc188288f47cef2e735af36fd62d635f624
shelby_meng <m15645418939@163.com>
2020-10-10 18:14:58 +0800

    update

---
+++ src/pages/Test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button, Card, Input, message } from 'antd'
+
+export default () => {
+    return (
+        <div>
+            <Button>aasdfsdf</Button>
+        </div>
+    )
+}
